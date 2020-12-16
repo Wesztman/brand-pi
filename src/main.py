db9ae3f9a92e923e430a988c44bcbf983df34111
Carl Westman <carl.westman91@gmail.com>
2020-12-16 08:42:48 +0100

    Added teensy sim in seperate thread

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import os, logging, threading, pty
+import os, logging, threading, pty, sys, signal
 from serial import Serial
 from transitions import Machine
 from datetime import datetime
@@ -9,79 +9,23 @@ from logging.handlers import RotatingFileHandler
 from robust_serial import write_order, Order, write_i8, write_i16, read_i8, read_order
 from robust_serial.utils import open_serial_port
 
-
+# Self defined imports
 from robot_state_machine.robot_state_machine import RobotStateMachine
 from config_handler.config_handler import GetConfiguration, config_file_listener
+from serial_handler.serial_handler import teensy_sim, serial_command
 from test_serial.test_serial import test_serial
 
 
-def serial_listener(port):
-    while True:
-        res = b""
-        while not res.endswith(b"\r\n"):
-            # Keep reading one byte at a time until we have a full line
-            res += os.read(port, 1)
-        logging.info("command: %s" % res)
-
-        # Write back the response
-        if res == b"QPGS\r\n":
-            os.write(port, b"correct result\r\n")
-        else:
-            os.write(port, b"I dont understand\r\n")
+def signal_handler(sig, frame):
+    logging.info("Exiting")
+    sys.exit(0)
 
 
 def main():
-    """ General declerations """
-    # Create general thread list
-    threads = []
 
-    # Variables for state enter actions
-    current_state = ""
-    previous_state = ""
-
-    # Create state machine object
+    """ State machine decleration """
+    # Create robot state machine object
     robot = RobotStateMachine()
-
-    """ Config file handling """
-    # Initialize config parser
-    current_folder = os.path.dirname(os.path.abspath(__file__))
-    config_file = os.path.join(current_folder, "config.ini")
-    get_config = GetConfiguration(config_file)
-
-    # Initialize config file listener thread
-    config_file_listener_thread = threading.Thread(
-        target=config_file_listener, args=[config_file]
-    )
-    threads.append(config_file_listener_thread)
-    config_file_listener_thread.start()
-
-    """ Serial communication """
-    # Create pseudoterminals for serial testing
-    master, slave = pty.openpty()
-    slave_name = os.ttyname(slave)
-
-    # Initialize serial listener thread
-    serial_listener_thread = threading.Thread(target=serial_listener, args=[master])
-    threads.append(serial_listener_thread)
-    serial_listener_thread.start()
-
-    # Open serial connection to the slave
-    ser = Serial(slave_name, 9600, timeout=1)
-
-    # Write
-
-    """ Logging """
-    # Initialize logger
-    logging.basicConfig(
-        level=get_config.as_int("logging", "log_level"),
-        format="%(asctime)s [%(levelname)s] %(message)s",
-        handlers=[
-            RotatingFileHandler("logs/debug.log", maxBytes=1000000, backupCount=5),
-            logging.StreamHandler(),
-        ],
-    )
-
-    # Initialize signal handler
 
     """ Main loop start """
     while True:
@@ -90,14 +34,60 @@ def main():
 
         # ======================================================================== #
         if robot.state == "init":
+            """ General declerations """
+            # Create general thread list
+            threads = []
 
-            if previous_state != robot.state:  # State enter actions, run once
-                pass
+            # Variables for state enter actions
+            current_state = ""
+            previous_state = ""
 
-            sleep(1.0)
-            robot.init_done()
+            """ Config file handling """
+            # Initialize config parser
+            current_folder = os.path.dirname(os.path.abspath(__file__))
+            config_file = os.path.join(current_folder, "config.ini")
+            get_config = GetConfiguration(config_file)
+
+            # Start config file listener thread
+            config_file_listener_thread = threading.Thread(
+                target=config_file_listener, args=[config_file]
+            )
+            threads.append(config_file_listener_thread)
+            config_file_listener_thread.start()
+
+            """ Serial communication """
+            # Create pseudoterminals for serial testing
+            master, slave = pty.openpty()
+            slave_name = os.ttyname(slave)
+
+            # Start teensy simulator thread
+            teensy_sim_thread = threading.Thread(target=teensy_sim, args=[master])
+            threads.append(teensy_sim_thread)
+            teensy_sim_thread.start()
+
+            # Open serial connection to the slave device
+            slave_device = Serial(slave_name, 9600, timeout=1)
+
+            """ Logging """
+            # Initialize logger
+            logging.basicConfig(
+                level=get_config.as_int("logging", "log_level"),
+                format="%(asctime)s [%(levelname)s] %(message)s",
+                handlers=[
+                    RotatingFileHandler(
+                        "logs/debug.log", maxBytes=1000000, backupCount=5
+                    ),
+                    logging.StreamHandler(),
+                ],
+            )
+
+            # Initialize signal handler
+            signal.signal(signal.SIGINT, signal_handler)
 
             # Create data sets
+
+            # Init done, transition to idle
+            robot.init_done()
 
         # ======================================================================== #
         elif robot.state == "idle":
@@ -105,12 +95,9 @@ def main():
             if previous_state != robot.state:  # State enter actions, run once
                 pass
 
-            sleep(1)
-            robot.start()
-
-            # Wait for start
-
-            # Go to Working
+                """ Check for start button """
+                # logging.info("Start button pressed")
+                robot.start()
 
         # ======================================================================== #
         elif robot.state == "working":
@@ -118,13 +105,21 @@ def main():
             if previous_state != robot.state:  # State enter actions, run once
                 pass
 
-            sleep(1.0)
-            robot.stop()
-
             """ Internal working loop """
 
             # -------------------------------------------------------------------------#
             # Internal Working State: Read Input
+            read_result = serial_command(slave_device, "right_distance")
+            logging.info(read_result)
+            sleep(1)
+
+            read_result = serial_command(slave_device, "left_distance")
+            logging.info(read_result)
+            sleep(1)
+
+            read_result = serial_command(slave_device, "front_distance")
+            logging.info(read_result)
+            sleep(1)
 
             # -------------------------------------------------------------------------#
             # Internal Working State: Data Processing
@@ -135,7 +130,10 @@ def main():
             # -------------------------------------------------------------------------#
             # Internal Working State: Cleanup -> Go to Read Input
 
-            # Go to Cleanup
+            """ Check for stop button """
+            # logging.info("Stop button pressed")
+            # Got to cleanup
+            # robot.stop()
 
         # ======================================================================== #
         elif robot.state == "cleanup":
@@ -143,12 +141,10 @@ def main():
             if previous_state != robot.state:  # State enter actions, run once
                 pass
 
-            sleep(1.0)
-            robot.reset()
-
             # Cleanup
 
             # Go to Idle
+            robot.reset()
 
         else:
             logging.warning("Unhandled robot state")
