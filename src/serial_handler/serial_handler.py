d6348b17364a8b896febe4abea8224f6d173e48a
Carl Westman <carl.westman91@gmail.com>
2020-12-17 21:24:13 +0100

    Removed teensy sim from serial handler

--- src/serial_handler/serial_handler.py
+++ src/serial_handler/serial_handler.py
@@ -1,23 +1,4 @@
-import logging, os
-
-
-def teensy_sim(port):
-    while True:
-        res = b""
-        while not res.endswith(b"\r\n"):
-            # Keep reading one byte at a time until we have a full line
-            res += os.read(port, 1)
-
-        res = res.rstrip()
-        logging.info("*Teensy sim* Command received: %s" % res)
-
-        # Write back the response
-        if res == b"right_distance":
-            os.write(port, b"*Teensy sim* 100\r\n")
-        elif res == b"left_distance":
-            os.write(port, b"*Teensy sim* 50\r\n")
-        else:
-            os.write(port, b"*Teensy sim* Incorrect command\r\n")
+import logging, os, threading
 
 
 def serial_command(ser, data):
