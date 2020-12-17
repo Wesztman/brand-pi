f97515e3e979c58e660b31ef8f119cb7723a2cec
Carl Westman <carl.westman91@gmail.com>
2020-12-17 21:21:24 +0100

    Created teensy sim class

---
+++ src/teensy_sim/teensy_sim.py
@@ -0,0 +1,31 @@
+import logging, os, threading, pty
+
+
+class TeensySim(threading.Thread):
+    def __init__(self):
+        # Create pseudoterminals for serial testing
+        master, slave = pty.openpty()
+        self.master_name = master
+        self.slave_name = os.ttyname(slave)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        while True:
+            res = b""
+            while not res.endswith(b"\r\n"):
+                # Keep reading one byte at a time until we have a full line
+                res += os.read(self.master_name, 1)
+
+            res = res.rstrip()
+            logging.info("*Teensy sim* Command received: %s" % res)
+
+            # Write back the response
+            if res == b"right_distance":
+                os.write(self.master_name, b"*Teensy sim* 100\r\n")
+            elif res == b"left_distance":
+                os.write(self.master_name, b"*Teensy sim* 50\r\n")
+            else:
+                os.write(self.master_name, b"*Teensy sim* Incorrect command\r\n")
+
+    def get_slave_name(self):
+        return self.slave_name
